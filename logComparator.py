0e95d4d196d45578416a709df8f40c27d0bde0e6
HectorDD <hectordavid1228@gmail.com>
2019-10-09 18:50:43 -0500

    changes for html report

--- logComparator.py
+++ logComparator.py
@@ -1,46 +1,57 @@
 import json
+import os
+import webbrowser
 
 exclusionList=["sessionId","clientReferenceId"]
 
 def identation(level):
     s=""
     for i in range(level):
-        s+="   "
+        s+="&nbsp;&nbsp;&nbsp;&nbsp;"
     return s
 
 def printLine(s,conflicts,key):
     if key in conflicts["wrong"]:
-        print(s+" [WRONG]")
+        return "<b><div style='color:red'>"+s+" [WRONG]</div></b>"
     elif key in conflicts["nonexits"]:
-        print(s+" [NONEXISTS]")
+        return "<b><div style='color:orange'>"+s+" [NONEXISTS]</div></b>"
     else:
-        print(s)
+        return "<div>"+s+"</div>"
 
-def generateHTML(jsonFile,conflicts,key=None):
+def generateHTML(jsonFile1,jsonFile2):
     f = open('holamundo.html','w')
-    content="<html><header></header><body><table width=100%><tr><td>"
-
+    conflicts1=compareLogs(jsonFile1,jsonFile2)
+    conflicts2=compareLogs(jsonFile2,jsonFile1)
+    content="<html><header><style>table {border: 1px solid DarkOrange;border-radius: 13px; border-spacing: 0;}</style></header><body><center><h2>JSON Comparator Report</h2></center><br><center><table border='1' width=90% cellpadding='10'><tr><td valign='top'>"
+    content+=printJson(jsonFile1,conflicts1)
     content+="</td><td>"
-
+    content+=printJson(jsonFile2,conflicts2)
     content+="</td></tr></table></body></html>"
+    f.write(content)
+    url='file://' +os.path.realpath(f.name)
+    f.close()
+    print("HTML Report in:",url)
+    webbrowser.open(url,new=2)
 
 
-def printJson(jsonFile,conflicts,key=None):
+def printJson(jsonFile,conflicts,key=None,result=""):
     if key==None:
         key=[]
-    for i in jsonFile:
-        if isinstance(jsonFile[i],dict):
-            s=identation(len(key))+i+": "
-            printLine(s,conflicts,key+[i])
-            printJson(jsonFile[i],conflicts,key+[i])
-        elif isinstance(jsonFile[i],list):
-            s=identation(len(key))+i+": "
-            printLine(s,conflicts,key+[i])
-            for j in jsonFile[i]:
-                printJson(j,conflicts,key+[i])
-        else:
-            s=identation(len(key))+i+": "+str(jsonFile[i])
-            printLine(s,conflicts,key+[i])
+    if not isinstance(jsonFile,str):
+        for i in jsonFile:
+            if isinstance(jsonFile[i],dict):
+                s=identation(len(key))+i+": "
+                result+=printLine(s,conflicts,key+[i])+"<br>"
+                result+=printJson(jsonFile[i],conflicts,key+[i])
+            elif isinstance(jsonFile[i],list):
+                s=identation(len(key))+i+": "
+                result+=printLine(s,conflicts,key+[i])+"<br>"
+                for j in jsonFile[i]:
+                    result+=printJson(j,conflicts,key+[i])
+            else:
+                s=identation(len(key))+i+": "+str(jsonFile[i])
+                result+=printLine(s,conflicts,key+[i])+"<br>"
+    return result
             
 
 def extractJson(file):
@@ -76,8 +87,8 @@ def compareLogs(json1,json2,key=None,conflicts=None):
         key=[]
     if not isinstance(json1,dict) or isinstance(json1,list):
         if json1!=json2:
-            conflicts["wrong"].append(key+[j])
-            print("is wrong in ",key+[j])
+            conflicts["wrong"].append(key)
+            print("is wrong in ",key)
             return 1
         else:
             return 0
@@ -87,22 +98,22 @@ def compareLogs(json1,json2,key=None,conflicts=None):
                 compareLogs(json1[j],json2[j],key+[j],conflicts)
             except:
                 conflicts["nonexits"].append(key+[j])
-                print("the following key has problems: ", key+[j]) 
+                #print("the following key has problems: ", key+[j]) 
         elif isinstance(json1[j],list):
             for i in range(len(json1[j])):
                 try:
                     compareLogs(json1[j][i],json2[j][i],key+[j],conflicts)
                 except:
                     conflicts["nonexits"].append(key+[j])
-                    print("the following key has problems: ",key+[j]) 
+                    #print("the following key has problems: ",key+[j]) 
         else:
             try:
                 if json1[j] != json2[j] and not j in exclusionList:
                     conflicts["wrong"].append(key+[j])
-                    print("is wrong in ",key+[j])
+                    #print("is wrong in ",key+[j])
             except:
                 conflicts["nonexits"].append(key+[j])
-                print("the following key has problems: ",key+[j])
+                #print("the following key has problems: ",key+[j])
     return conflicts
                 
 def compareTestCases(testCases):
@@ -115,18 +126,22 @@ def compareTestCases(testCases):
         for j in range(len(log1)):
             print("INDEX: "+str(j))
             conflicts=compareLogs(log1[j],log2[j])
-            printJson(log1[j],conflicts)
+            print(printJson(log1[j],conflicts))
         print("Checking "+i+"on < "+i+"off")
         
         for j in range(len(log2)):
             print("INDEX: "+str(j))
             conflicts=compareLogs(log2[j],log1[j])
-            printJson(log2[j],conflicts)
-        
+            print(printJson(log2[j],conflicts))
+
         
 testCases=["logh"]
 
-compareTestCases(testCases)
+#compareTestCases(testCases)
+#log1=loadLog("loghon")[0]
+#log2=loadLog("loghoff")[0]
+#generateHTML(log1,log2)
+
             
             
             
